09e5cc03e122179a9bd358ab800f648390f1bfad
Kenzie Togami <ket1999@gmail.com>
2014-06-24 19:17:33 -0700

    Moved js folder

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Builds Manager © 2014 TechShroom Studios
+define(["jquery"], function ($) {
+    var appns = {};
+    (function () {
+        appns.init = function () {
+            console.log($);
+        };
+    })();
+    return appns;
+});
