f0c34d98ff11a18c52d08b68d213eceb66cc6609
Kenzie Togami <ket1999@gmail.com>
2014-08-07 22:35:24 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@
 define(["jquery" ,"xdomain.require.js"], function ($, xd) {
     $.ajax = xd.wrapAjax($);
     var appns = {};
-    (function () {
-        appns.init = function () {
-            $.post( "http://techshroom.com/non-wp/uploads/manager/commit.php", { 'r|e|p|o': BUILD_DATA.repo, 'o|w|n|er': BUILD_DATA.owner, 'r|e|p|odir': BUILD_DATA.dir})
-                .done(function(data) {
-                    $('#builds-' + BUILD_DATA.owner + '-' + BUILD_DATA.repo).html(data);
-                });
-        };
-    })();
+    appns.init = function () {
+            var get = $.get(url);
+            var $div = $('#builds-' + BUILD_DATA.owner + '-' + BUILD_DATA.repo);
+            get.done(function (data) {
+                $div.html($.parseHTML(data.results[0]));
+            });
+            get.fail(function () {
+                $div.text("Failed GET from " + url);
+            });
+    }
     return appns;
 });
