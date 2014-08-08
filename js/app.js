49a7a4fb6a6fe58ea8ad91ede0a37da27ed35f50
Kenzie Togami <ket1999@gmail.com>
2014-08-07 18:04:58 -0700

    This should be it

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // Builds Manager © 2014 TechShroom Studios
-define(["jquery"], function () {
+define(["jquery"], function ($) {
     var appns = {};
     (function () {
         appns.init = function () {
-            console.log(BUILD_DATA);
+            $.post( "http://techshroom.com/non-wp/uploads/manager/commit.php", { 'r|e|p|o': BUILD_DATA.repo, 'o|w|n|er': BUILD_DATA.owner, 'r|e|p|odir': BUILD_DATA.dir})
+                .done(function(data) {
+                    $('#builds-' + BUILD_DATA.owner + '-' + BUILD_DATA.repo).html(data);
+                });
         };
     })();
     return appns;
