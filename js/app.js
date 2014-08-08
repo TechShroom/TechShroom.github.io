8546058a0bf0fac92e22fa17c8823b398e4924bd
Kenzie Togami <ket1999@gmail.com>
2014-08-08 16:52:43 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,28 @@
 define(["jquery" ,"js/xdomain.require.js"], function ($, xd) {
     $.ajax = xd.wrapAjax($);
     var appns = {};
+    function toggleFunc($it) {
+        // $it -> button with span icon
+        $it.children("span").toggleClass("glyphicon-plus glyphicon-minus");
+    };
     appns.init = function () {
-        $('.build-div').each(function (i, it) {
+        var divs = $('.build-div');
+        divs.each(function (i, it) {
             var $it = $(it);
             var dir = $it.attr('rdir');
             console.log('http://techshroom.com/non-wp/uploads/travis-ci/commital/' + dir + '/links.html');
             var get = $.get('http://techshroom.com/non-wp/uploads/travis-ci/commital/' + dir + '/links.html');
             get.done(function (data) {
+                $it.hide();
                 $it.html($.parseHTML(data.results[0]));
             });
             get.fail(function () {
                 $it.text("Failed GET from " + url);
             });
+        });
+        var btns = $('button.build-toggle');
+        btns.click(function () {
+            toggleFunc($(this));
         });
     }
     return appns;
