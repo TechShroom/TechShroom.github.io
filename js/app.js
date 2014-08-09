735c4c90215a172fb14bd99b5782db9e7c2e5e27
Kenzie Togami <ket1999@gmail.com>
2014-08-09 13:12:35 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ define(["jqui" ,"js/xdomain.require.js"], function ($undef, xd) {
                                                             '">Latest ('+
                                                             firstLiText+
                                                             ")</a> "+
-                                                            $(firstLi).children("p")+
+                                                            $(firstLi).children("p").html()+
                                                             "</li>";
                     var parse = $.parseHTML(html);
                     console.log(parse);
