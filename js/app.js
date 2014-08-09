990fd0bdde7f6ec93e52409a62c7c62f0ce8deaa
Kenzie Togami <ket1999@gmail.com>
2014-08-08 17:39:31 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ define(["jquery" ,"js/xdomain.require.js"], function ($, xd) {
     function toggleFunc($it) {
         // $it -> button with span icon
         $it.children("span").toggleClass("glyphicon-plus glyphicon-minus");
-        console.log($it.parent().next()[0]);
         $it.parent().next().toggle(400);
     };
     appns.init = function () {
@@ -13,13 +12,15 @@ define(["jquery" ,"js/xdomain.require.js"], function ($, xd) {
         divs.each(function (i, it) {
             var $it = $(it);
             var dir = $it.attr('rdir');
-            console.log('http://techshroom.com/non-wp/uploads/travis-ci/commital/' + dir + '/links.html');
             var get = $.get('http://techshroom.com/non-wp/uploads/travis-ci/commital/' + dir + '/links.html');
             get.done(function (data) {
                 $it.hide();
                 $it.html($.parseHTML(data.results[0]));
                 $it.children("ul").addClass("list-group");
-                $it.children("ul").children("li").addClass("list-group-item");
+                $it.children("ul").each(function (i, it2){
+                    var $it2 = $(it2);
+                    $it2.children("li").addClass("list-group-item");
+                }
             });
             get.fail(function () {
                 $it.text("Failed GET from " + url);
