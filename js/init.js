a906012a6cc28adb2f7db3c9c4a845fcba22e880
Kenzie Togami <ket1999@gmail.com>
2014-08-08 18:16:40 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ requirejs.config({
     // "enforceDefine": true,
     "paths": {
         "jquery": ["//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"],
-        "jqui": ["//code.jquery.com/ui/1.11.0/jquery-ui.min.js"]
+        "jqui": ["//code.jquery.com/ui/1.11.0/jquery-ui.min"]
     },
     shim: {
         "jqui": {
