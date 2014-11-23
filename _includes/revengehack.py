7fbd9299e70571989a1dbe458a878e883674fb11
Kenzie Togami <ket1999@gmail.com>
2014-11-23 11:57:04 -0800

    Make it actually work

--- _includes/revengehack.py
+++ _includes/revengehack.py
@@ -1,8 +1,8 @@
 import gmpy2
 from gmpy2 import mpz
-from hash import sha1
+from hashlib import sha1
 message = "cat flag" # ls first time
-messagesha = sha1(message).hexdigest() # sha1 of message
+messagesha = sha1(message.encode("ascii")).hexdigest() # sha1 of message
 targetsig = "0001" + "f" * 8 + "00" + messagesha + "f" * 714 # target signature
 targethex = int(targetsig, 16) # target signature, converted to an int
 icbrt = gmpy2.iroot(mpz(targethex), 3) # integer cube root
